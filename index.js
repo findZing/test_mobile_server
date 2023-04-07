cdafcd0c2799879c6a924913bf7066442717750f
Manh <manhnguyenhuu1001@gmail.com>
2023-04-07 17:26:55 +0700

    Change

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(fileUpload());
 
 app.use(cors())
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
